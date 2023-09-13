370b32d6f838df705b0695f957f0b096f1b7b015
Aaron Zhao <aaron-zhao15@users.noreply.github.com>
2023-09-13 16:30:02 -0400

    find_object switcheroo

--- Lab 1/find_object.py
+++ Lab 1/find_object.py
@@ -5,18 +5,8 @@ import argparse
 import time
 import imutils
 
-ballLower = (0, 110, 0)
-ballUpper = (15, 255, 255)
-
-# cv2.namedWindow('mask')
-
-# cv2.createTrackbar('HMin', 'mask', 0, 179, lambda x: None)
-# cv2.createTrackbar('SMin', 'mask', 0, 255, lambda x: None)
-# cv2.createTrackbar('VMin', 'mask', 0, 255, lambda x: None)
-# cv2.createTrackbar('HMax', 'mask', 0, 179, lambda x: None)
-# cv2.createTrackbar('SMax', 'mask', 0, 255, lambda x: None)
-# cv2.createTrackbar('VMax', 'mask', 0, 255, lambda x: None)
-
+ballLower = (0, 150, 0)
+ballUpper = (10, 255, 255)
 def get_circles(input_frame):
     
     frame = input_frame.copy()
@@ -27,17 +17,23 @@ def get_circles(input_frame):
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
     
     mask = cv2.inRange(hsv, ballLower, ballUpper)
+
+    open_struct = cv2.getStructuringElement(cv2.MORPH_RECT,(19,19))
+    close_struct = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
     
-    cv2.imshow('mask', mask)    
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, close_struct)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, open_struct)
     
+    mask = cv2.GaussianBlur(mask, (15, 15), 2, 2)
+
     circles = cv2.HoughCircles(mask, 
         cv2.HOUGH_GRADIENT, 
         1, 
         mask.shape[0] / 4, 
         param1=40, 
         param2=20, 
-        minRadius=40, 
-        maxRadius=300)
+        minRadius=10, 
+        maxRadius=0)
     
     return circles, mask
 
@@ -99,40 +95,34 @@ while(1):
     ret, input_frame = vs.read()
     
     circles, mask = get_circles(input_frame)
-    # center, mask = get_circles_countours(input_frame)
+    center, mask = get_circles_countours(input_frame)
     
-    # if circles is not None and center[0] is not None:
+    if circles is not None and center[0] is not None:
         
-    #     center_circles = [circles[0, 0], circles[0, 1], circles[0, 2]]
-    #     center_contours = [center[0][0], center[0][1], center[1]]
-    #     centers = np.array([center_circles, center_contours])
-    #     center = np.average(centers, axis=0)
-    #     print(center)
-    #     cv2.circle(input_frame, 
-    #         center=(int(center[0]), int(center[1])), 
-    #         radius=int(center[2]), 
-    #         color=(0, 255, 0), 
-    #         thickness=2)
-    
-    if circles is not None and circles[0] is not None:
-        circles = circles[0][0]
-        circles = [circles[0], circles[1], circles[2]]
+        circles = circles[0]
+        center_circles = [circles[0, 0], circles[0, 1], circles[0, 2]]
+        center_contours = [center[0][0], center[0][1], center[1]]
+        centers = np.array([center_circles, center_contours])
+        center = np.average(centers, axis=0)
+        print(center)
         cv2.circle(input_frame, 
-            center=(int(circles[0]), int(circles[1])), 
-            radius=int(circles[2]), 
+            center=(int(center[0]), int(center[1])), 
+            radius=int(center[2]), 
             color=(0, 255, 0), 
             thickness=2)
+        
+    # if circles[0] is not None:
+    #     cv2.circle(input_frame, 
+    #         center=(circles[0][0], circles[0][1]), 
+    #         radius=circles[1], 
+    #         color=(0, 255, 0), 
+    #         thickness=2)
 
     # Display the resulting frame, quit with q
     cv2.imshow('frame', input_frame)
-    # cv2.imshow('frame', mask)
+    # cv2.imshow('mask', mask)
     # cv2.imwrite("color_mask.jpg", mask)
     out.write(input_frame)
-    
-    #ballLower = (cv2.getTrackbarPos('HMin', 'mask'), cv2.getTrackbarPos('SMin', 'mask'), cv2.getTrackbarPos('VMin', 'mask'))
-    #ballUpper = (cv2.getTrackbarPos('HMax', 'mask'), cv2.getTrackbarPos('SMax', 'mask'), cv2.getTrackbarPos('VMax', 'mask'))
-    
-    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
