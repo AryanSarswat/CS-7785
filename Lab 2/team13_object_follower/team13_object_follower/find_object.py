8c2655dfb80f2fde47b15d7d12db386322d2e8f7
asarswat8 <asarswat8@gatech.edu>
2023-09-25 10:29:31 -0400

    Create node for obtaining image and publishing object coord

--- Lab 2/team13_object_follower/team13_object_follower/find_object.py
+++ Lab 2/team13_object_follower/team13_object_follower/find_object.py
@@ -1,23 +1,94 @@
 # Team - Aaron Zhao, Aryan Sarswat
 
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import CompressedImage
+from rclpy.qos import QoSProfile, QoSDurabilityPolicy, QoSReliabilityPolicy, QoSHistoryPolicy
+from std_msgs.msg import String
+
+import sys
 
 import numpy as np
 import cv2
-from collections import deque
-import argparse
-import time
+from cv_bridge import CvBridge
 import imutils
 
-class DetectObject:
+class DetectObject(Node):
     def __init__(self):
+        # Creates the node.
+        super().__init__('detect_object_node')
+        
         self.ballLower = (0, 150, 0)
         self.ballUpper = (15, 255, 255)
-        self.video_cap = cv2.VideoCapture(0)
         
-        self.frame_height = int(self.video_cap.get(4))
-        self.frame_width = int(self.video_cap.get(3))
-        self.video_writer = cv2.VideoWriter('outpy2.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (self.frame_width,self.frame_height))
+        self.declare_parameter('show_image_bool', True)
+        self.declare_parameter('window_name', "Raw Image")
+        
+        #Determine Window Showing Based on Input
+        self._display_image = bool(self.get_parameter('show_image_bool').value)
+        
+        # Declare some variables
+        self._titleOriginal = self.get_parameter('window_name').value # Image Window Title
+        if(self._display_image):
+            # Set Up Image Viewing
+            cv2.namedWindow(self._titleOriginal, cv2.WINDOW_AUTOSIZE )
+            cv2.moveWindow(self._titleOriginal, 50, 50)
+        
+        #Set up QoS Profiles for passing images over WiFi
+        image_qos_profile = QoSProfile(depth=5)
+        image_qos_profile.history = QoSHistoryPolicy.KEEP_LAST
+        image_qos_profile.durability = QoSDurabilityPolicy.VOLATILE
+        image_qos_profile.reliability = QoSReliabilityPolicy.BEST_EFFORT
+        
+        #Declare that the minimal_video_subscriber node is subcribing to the /camera/image/compressed topic.
+        self._video_subscriber = self.create_subscription(
+                CompressedImage,
+                '/image_raw/compressed',
+                self._image_callback,
+                image_qos_profile)
+        self._video_subscriber # Prevents unused variable warning.
+        
+        # Create Publisher
+        self.publisher = self.create_publisher(String, 'object_location', 10)
     
+    def _image_callback(self, CompressedImage):
+        # The "CompressedImage" is transformed to a color image in BGR space and is store in "_imgBGR"
+        self._imgBGR = CvBridge().compressed_imgmsg_to_cv2(CompressedImage, "bgr8")
+        if(self._display_image):
+            # Display the image in a window
+            input_frame = self._imgBGR.copy()
+            
+            center, radius, pre_processed_frame = self.detect_object(input_frame)
+            
+            if center is not None:
+                cv2.circle(input_frame, 
+                    center=(int(center[0]), int(center[1])), 
+                    radius=int(radius), 
+                    color=(0, 255, 0), 
+                    thickness=2)
+                
+                msg = String()
+                msg.data = str(center[0]) + "," + str(center[1])
+                self.publisher.publish(msg)
+                self.get_logger().info('Publishing: "%s"' % msg.data)
+        
+            self.show_image(input_frame)
+            
+    
+    def get_image(self):
+        return self._imgBGR
+    
+    def get_user_input(self):
+        return self._user_input
+    
+    def show_image(self, img):
+        cv2.imshow(self._titleOriginal, img)
+        
+        self._user_input = cv2.waitKey(1)
+        if self._user_input == ord('q'):
+            cv2.destroyAllWindows()
+            raise SystemExit
+        
     def pre_process(self, input_frame):
         frame = input_frame.copy()
 
@@ -73,31 +144,20 @@ class DetectObject:
         pre_processed_frame = self.pre_process(input_frame)
         center, radius = self.get_contours(pre_processed_frame)
         return center, radius, pre_processed_frame
-    
-    def start(self):
-        
-        while True:
-            ret, input_frame = self.video_cap.read()
-            
-            center, radius, pre_processed_frame = self.detect_object(input_frame)
-            
-            if center is not None:
-                cv2.circle(input_frame, 
-                    center=(int(center[0]), int(center[1])), 
-                    radius=int(radius), 
-                    color=(0, 255, 0), 
-                    thickness=2)
-                
-            cv2.imshow('frame', input_frame)
-            self.video_writer.write(input_frame)
-            
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        
-        self.video_writer.release()
-        self.video_cap.release()
 
-if __name__ == '__main__':
+
+def main():
+    rclpy.init()
     object_detector = DetectObject()
-    object_detector.start()
-    cv2.destroyAllWindows()
+    
+    try:
+        rclpy.spin(object_detector)
+    except SystemExit:
+        rclpy.logging.get_logger("Object Detector Node Info...").info("Shutting Down")
+    
+    detect_object.destroy_node()
+    rclpy.shutdown()
+    
+    
+if __name__ == '__main__':
+    main()
