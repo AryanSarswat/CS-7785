186ce0a251d19dabdb9af43dd00d38ff1cc8fc53
asarswat8 <asarswat8@gatech.edu>
2023-11-17 10:28:19 -0500

    Add extra images

--- lab_6/lab6_supplemental/classifier.py
+++ lab_6/lab6_supplemental/classifier.py
@@ -3,19 +3,52 @@ import sys
 import csv
 import time
 import numpy as np
+from sklearn.cluster import KMeans
+
+COLOR_MAPS = {
+    'green' : [147, 72.6, 28.6],
+    'red' : [355, 67.7, 62.0],
+    'blue' : [233, 58.6, 22.7],
+}
+
 
 # Hyperparameters
-IMAGE_HEIGHT = 64
-IMAGE_WIDTH = 64
+IMAGE_HEIGHT = 128
+IMAGE_WIDTH = 128
+NUM_CHANNELS = 3
+NUM_CLASSES = 6
 IMAGE_SIZE = (IMAGE_HEIGHT, IMAGE_WIDTH)
 
 def preprocess(img):
     # Isolate the region of interest
     # TODO: Isolate based on color
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     
-    img = cv2.resize(img, (IMAGE_SIZE))
+    masked_imgs = []
     
-    return image
+    # Get all pixels that are within the color range
+    for mask in COLOR_MAPS:
+        h, s, v = COLOR_MAPS[mask]
+        
+        upper = np.array([h + 10, s + 10, v + 10])
+        lower = np.array([h - 10, s - 10, v - 10])
+        
+        mask = cv2.inRange(hsv, lower, upper)
+        masked_imgs.append(cv2.bitwise_and(img, img, mask=mask))
+    
+    # Get the largest region of interest
+    largest = cv2.bitwise_or(masked_imgs[0], masked_imgs[1])
+    largest = cv2.bitwise_or(largest, masked_imgs[2])
+    
+    cv2.imshow("Largest", largest)
+    cv2.waitKey()
+    
+    roi = cv2.bitwise_and(img, img, mask=largest)
+    
+    cv2.imshow("ROI", roi)
+    cv2.waitKey()
+        
+    return bilateral
 
 ### Load training images and labels
 imageDirectory = './2022Fimgs/'
@@ -26,7 +59,8 @@ with open(imageDirectory + 'train.txt', 'r') as f:
 
 train = []
 for i in range(len(lines)):
-    img = cv2.imread(imageDirectory + lines[i][0] + ".png", 0)
+    # Get color image
+    img = cv2.imread(imageDirectory + lines[i][0] + ".png", 1 if NUM_CHANNELS == 3 else 0)
     img = preprocess(img)
     train.append(img)
 
@@ -54,8 +88,8 @@ correct = 0.0
 confusion_matrix = np.zeros((6,6))
 
 for i in range(len(lines)):
-    original_img = cv2.imread(imageDirectory+lines[i][0]+".png",0)
-    test_img = np.array(preprocess(cv2.imread(imageDirectory+lines[i][0]+".png",0),IMAGE_SIZE))
+    original_img = cv2.imread(imageDirectory+lines[i][0]+".png", 1 if NUM_CHANNELS == 3 else 0)
+    test_img = np.array(preprocess(original_img))
     if(__debug__):
         cv2.imshow(Title_images, original_img)
         cv2.imshow(Title_resized, test_img)
